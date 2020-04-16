81950f6cb1f37ff93e657fa709b807295cecb8a6
Jesus Hernandez <jihernandez9513@gmail.com>
2020-04-15 18:01:46 -0700

    logic fixes and added rks_notation

--- rksolver.py
+++ rksolver.py
@@ -30,6 +30,33 @@ from time import sleep, localtime
 import RPi.GPIO as GPIO
 import asyncio
 
+RKS_NOTATION = {'F': 0,
+                'R': 0,
+                'U': 0,
+                'L': 0,
+                'B': 0,
+                'D': 0,
+                'F\'': 0,
+                'R\'': 0,
+                'U\'': 0,
+                'L\'': 0,
+                'B\'': 0,
+                'D\'': 0,
+                'F2': 0,
+                'R2': 0,
+                'U2': 0,
+                'L2': 0,
+                'B2': 0,
+                'D2': 0
+                }
+
+
+def gpio_init():
+    # reference pins by physical pin number
+    # avoids compatibility issues between pi board revisions
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)  # remove annoying debugger issues
+
 
 class motor:
     '''motor object to describe setup and functions'''
@@ -48,31 +75,41 @@ class motor:
     # TODO: map steps to degrees of rotation
     # BUG: GPIO.output needs to support multiple axis and subsequently multiple IO in different directions
     # NOTE: GPIO.output([list of pins], [list of state]) works to turn on multiple ports with different states
-    def rotate(self, steps, choice):
-        print(localtime())
+    def rotate(self, steps=10, choice='cw'):
+        ''' steps is number of times the motor toggles the step pin
+            choices are 'cw' or 'ccw' as string
+        '''
         if choice in self.directions:
             GPIO.output(self.dir_pin, self.directions[choice])
-            while steps > 0:
-                print(steps)
-                GPIO.output([self.dir_pin, self.step_pin], [True, False])
-                sleep(self.step_delay)
-                GPIO.output([self.dir_pin, self.step_pin], [False, True])
-                sleep(self.step_delay)
-                steps -= 1
-
         else:
             raise Exception('invalid direction input')
-        print(localtime())
+
+        # routine to toggle step pin
+        while steps > 0:
+            GPIO.output(self.step_pin, True)
+            sleep(self.step_delay)
+            GPIO.output(self.step_pin, False)
+            sleep(self.step_delay)
+            steps -= 1
 
 
-def gpio_init():
-    # reference pins by physical pin number
-    # avoids compatibility issues between pi board revisions
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setwarnings(False)  # remove annoying debugger issues
+class axis(motor):
+    ''' 2 motor per axis '''
 
-    # TODO: populate this list with the pin configuration
-    stepper_pins = []  # list to hold dedicated stepper pins
+    def __init__(self, motor_a=None, motor_b=None):
+        self.MotorA = motor_a
+        self.MotorB = motor_b
+
+    def move_axis(self, open_close=None):
+        if open_close == 'in':
+            self.MotorA.rotate()
+            self.MotorB.rotate()
+        elif open_close == 'out':
+            self.MotorA.rotate()
+            self.MotorB.rotate()
+        else:
+            raise ValueError(
+                "innappropriate value\n in or out needs to be specified")
 
 
 def init_modules():
@@ -85,19 +122,24 @@ def init_modules():
     # init GPIO
     gpio_init()
 
-    # init stepper motors
-    a = motor()
-    a.rotate(50, 'cw')
-    a.rotate(100, 'ccw')
     # init variables
-    print("async done")
 
 
 def main(args):
     init_modules()
-    return 0
+
+    # init stepper motors
+    m_front = motor()
+    m_back = motor()
+    m_left = motor()
+    m_right = motor()
+    m_xAxis = motor()
+    m_yAxis = motor()
+
+    while 1:
+        sleep(.1)
 
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+    sys.exit((main(sys.argv)))
